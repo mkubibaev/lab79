3a17b441d36d184e4a97ceead90ddf70d9508a50
mkubibaev <mkubibaev@gmail.com>
2019-03-28 17:55:56 +0600

    CRUD operations for places

---
+++ app/places.js
@@ -0,0 +1,78 @@
+const express = require('express');
+
+const createRouter = connection => {
+    const router = express.Router();
+
+    router.get('/', (req, res) => {
+        connection.query('SELECT `id`, `name` FROM `places`', (error, results) => {
+            if (error) {
+                return res.status(500).send({error: error.sqlMessage});
+            }
+
+            res.send(results);
+        })
+    });
+
+    router.get('/:id', (req, res) => {
+        connection.query('SELECT * FROM `places` WHERE `id` = ?', req.params.id, (error, results) => {
+            if (error) {
+                return res.status(500).send({error: error.sqlMessage});
+            }
+
+            if (results[0]) {
+                res.send(results[0]);
+            } else {
+                res.status(404).send({error: 'Place not found'});
+            }
+        });
+    });
+
+    router.post('/', (req, res) => {
+        const newPlace = req.body;
+
+        connection.query('INSERT INTO `places` (`name`, `description`) VALUES (?, ?)',
+            [newPlace.name, newPlace.description],
+            (error, results) => {
+                if (error) {
+                    return res.status(500).send({error: error.sqlMessage});
+                }
+
+                res.send({...newPlace, id: results.insertId});
+            });
+    });
+
+    router.delete('/:id', (req, res) => {
+        connection.query('DELETE FROM `places` WHERE `id` = ?', req.params.id, (error, results) => {
+            if (error) {
+                return res.status(500).send({error: error.sqlMessage});
+            }
+
+            res.send({message: 'place deleted!'});
+        });
+    });
+
+    router.put('/:id', (req, res) => {
+        const updatedPlace = req.body;
+        updatedPlace.id = req.params.id;
+
+        connection.query('UPDATE `places` SET name = ?, description = ? WHERE id = ?',
+            [updatedPlace.name, updatedPlace.description, updatedPlace.id],
+            (error, results) => {
+                if (error) {
+                    return res.status(500).send({error: error.sqlMessage});
+                }
+
+                if (results.affectedRows === 0) {
+                    return res.status(404).send({error: 'Place not found'})
+                }
+
+                res.send(updatedPlace);
+            });
+    });
+
+    return router;
+};
+
+
+
+module.exports = createRouter;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
 const categories = require('./app/categories');
+const places = require('./app/places');
 
 const app = express();
 const port = 8000;
@@ -17,6 +18,7 @@ const connection = mysql.createConnection({
 app.use(express.json());
 app.use(cors());
 app.use('/categories', categories(connection));
+app.use('/places', places(connection));
 
 connection.connect(err => {
     if (err) {
